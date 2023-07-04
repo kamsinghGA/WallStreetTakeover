ab40ce80bc84d1f636f8868fa04ab4f4197c24e2
kamsinghGA <kamaljitga@gmail.com>
2023-07-03 21:40:43 -0400

    updated the UI for the home page

---
+++ models/trader.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mngoose.Schema;
+
+const traderSchema = new Schema({
+    traderName: String,
+    tickerName: String,
+    
+})
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/traders', tradersRouterRouter);
+app.use('/traders', tradersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
